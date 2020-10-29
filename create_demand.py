98f59ca0cdae8a6b2a8525751a8f360b6663ef55
Ivan Terekh <i.terekh@andersenlab.com>
2020-10-29 10:06:40 +0100

    reformat

--- create_demand.py
+++ create_demand.py
@@ -13,10 +13,11 @@ import numpy as np
 
 def add_products(uni_param, a_days):
     tmp = []
-    
+
     for i in range(a_days):
         tmp.append(round(np.random.normal(uni_param[0], uni_param[1])))
     return tmp
+
 
 def create_demand_parameters(lb_avg_demand, ub_avg_demand, lb_dev, ub_dev):
     return [round(np.random.uniform(lb_avg_demand, ub_avg_demand)), round(np.random.uniform(lb_dev, ub_dev))]
@@ -25,20 +26,17 @@ def create_demand_parameters(lb_avg_demand, ub_avg_demand, lb_dev, ub_dev):
 # In[105]:
 
 
-df = pd.read_csv('full_info.csv', encoding = 'cp1251')
-
+df = pd.read_csv('full_info.csv', encoding='cp1251')
 
 # In[106]:
 
 
 df['key_adr'] = df['city'] + ' ' + df['street'] + ' ' + df['houseNumber']
 
-
 # In[107]:
 
 
 df = df.drop(columns=['countryCode', 'countryName', 'state', 'county', 'city', 'street', 'postalCode', 'houseNumber'])
-
 
 # In[108]:
 
@@ -54,12 +52,10 @@ list_demand = []
 
 for a in df['key_adr']:
     for p in range(products):
-        t1 = [a, 'product-' + str(p+1)]
+        t1 = [a, 'product-' + str(p + 1)]
         t2 = add_products(create_demand_parameters(lb_avg_dem[p], ub_avg_dem[p], lb_d[p], ub_d[p]), days)
         t1.extend(t2)
         list_demand.append(t1)
-    
-
 
 # In[109]:
 
@@ -67,10 +63,9 @@ for a in df['key_adr']:
 headers = ['key_adr', 'product']
 
 for i in range(days):
-    headers.append('day-' + str(i+1))
+    headers.append('day-' + str(i + 1))
 
-df_demand = pd.DataFrame(list_demand, columns = headers)
-
+df_demand = pd.DataFrame(list_demand, columns=headers)
 
 # In[110]:
 
@@ -78,39 +73,32 @@ df_demand = pd.DataFrame(list_demand, columns = headers)
 num = df_demand._get_numeric_data()
 num[num < 0] = 0
 
-
 # In[111]:
 
 
 df_demand = df_demand.set_index(['key_adr']).sort_index()
-
 
 # In[112]:
 
 
 df = df.set_index(['key_adr']).sort_index()
 
-
 # In[113]:
 
 
-total_demand = df_demand.join(df, on = ['key_adr'])
-
+total_demand = df_demand.join(df, on=['key_adr'])
 
 # In[114]:
 
 
 total_demand = total_demand.reset_index()
 
-
 # In[115]:
 
 
 total_demand = total_demand.set_index(['key_adr', 'product']).sort_index()
 
-
 # In[116]:
 
 
 total_demand
-
--- getdata.py
+++ getdata.py
@@ -3,25 +3,27 @@ import requests
 import json
 import csv
 
-#апи ключ для инфы с HERE
+# апи ключ для инфы с HERE
 geocoderApi = herepy.GeocoderApi('bOZAeO552D42cNgIwLvUz0gVvU-JNWTFI9gmIXDL1qY')
 
-#инфа для получения адресов магазинов, заголовок такой чтобы наебывать сервера и нам возвращали ответ, а на блочили как ботов (на всякий случай)
+# инфа для получения адресов магазинов, заголовок такой чтобы наебывать сервера и нам возвращали ответ, а на блочили как ботов (на всякий случай)
 url = 'https://evroopt.by/wp-content/themes/evroopt/klaster-shop-load.php?magazine=5&city=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&type_shop='
-headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Mobile Safari/537.36'}
-r = requests.get(url, headers = headers)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Mobile Safari/537.36'}
+r = requests.get(url, headers=headers)
 
-#парсим json ответа
+# парсим json ответа
 dd = json.loads(r.text)
 
-#лист для записи адресов с сайта
+# лист для записи адресов с сайта
 shop_addresses = []
 
-#если честно, я хз зачем я пишу всё в файл, если потом все равно читаю из него, но пусть будет
+# если честно, я хз зачем я пишу всё в файл, если потом все равно читаю из него, но пусть будет
 for shop in range(len(dd['info'])):
     shop_addresses.append(dd['info'][shop]['city'])
 
-#иногда апи возвращает несколько вариантов на запрос - выбираем лучший по оценке результата
+
+# иногда апи возвращает несколько вариантов на запрос - выбираем лучший по оценке результата
 def find_best_item(rd):
     best_score = 0
     best_item = 0
@@ -31,7 +33,8 @@ def find_best_item(rd):
             best_item = item
     return best_item
 
-#отправка запросов в HERE
+
+# отправка запросов в HERE
 full_info = []
 for i in range(len(shop_addresses)):
     shop = shop_addresses[i].replace('Минск, ', '')
@@ -47,10 +50,11 @@ for i in range(len(shop_addresses)):
 
 filename = 'full_info.csv'
 
-#запись в файл, кодировка cp-1251 (наверн)
+# запись в файл, кодировка cp-1251 (наверн)
 with open(filename, "w", newline="") as file:
-    header = ['countryCode', 'countryName', 'state', 'county', 'city', 'street', 'postalCode', 'houseNumber', 'lat', 'lng']
-    writer = csv.DictWriter(file, fieldnames = header)
+    header = ['countryCode', 'countryName', 'state', 'county', 'city', 'street', 'postalCode', 'houseNumber', 'lat',
+              'lng']
+    writer = csv.DictWriter(file, fieldnames=header)
     writer.writeheader()
     for row in full_info:
         writer.writerow(row)
